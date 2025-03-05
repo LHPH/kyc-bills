f1aac42617f42dd2e920df47329617f99d0ef8e7
LHPH <luislyly@hotmail.com>
2025-03-05 10:56:30 -0600

    Upload basic project template

---
+++ src/main/java/com/kyc/bills/KycBillsApplication.java
@@ -0,0 +1,13 @@
+package com.kyc.bills;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KycBillsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KycBillsApplication.class, args);
+	}
+
+}
